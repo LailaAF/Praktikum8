e7fe2302d052902f1ec268f967c5d1d4091597fd
LailaAF <lailajoli89@gmail.com>
2022-06-20 13:11:22 +0700

    Praktikum8

---
+++ application/controllers/Auth.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->model('M_mahasiswa');
+	}
+
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/sidebar');
+		$this->load->view('template/footer');
+	}
+}
